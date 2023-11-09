fabc8e5fa3c4d25c68ef1418841391bce3b4b071
Uraani@live.fi <Uraani@live.fi>
2023-11-09 13:01:03 +0200

    fixed batch

--- compressor.bat
+++ compressor.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 REM This batch file opens the blender in background mode and runs a Python script
 REM The blender.exe is already in the environment variables
-py %~dp0\compressor.py
+py compressor.py
