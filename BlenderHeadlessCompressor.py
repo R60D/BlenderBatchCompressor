4a8c811dbed71e1a08a41ca621f713e546c33b8e
Uraani@live.fi <Uraani@live.fi>
2023-11-10 03:11:50 +0200

    Added jpg compression option

--- BlenderHeadlessCompressor.py
+++ BlenderHeadlessCompressor.py
@@ -23,6 +23,38 @@ with open(PicklePath, "rb") as f:
 
 SourceDir = dirs["source"]
 DestDir = dirs["dest"]
+JpgCompression = dirs["jpg"]
+
+
+def convert_packed_images_to_jpg():
+    # loop through all images in the blend file
+    for img in bpy.data.images:
+        
+        try:
+            # check if the image is packed and not already jpg
+            if img.packed_file:
+                # save the image as jpg with the given quality
+                img.unpack(method='USE_LOCAL')
+                img.file_format = 'JPEG'
+                img.save()
+                # unpack the image
+             
+                # reload the image from the temporary file
+ 
+                img.reload()
+                # pack the image again
+                img.pack()
+        except:
+            print(f"{img.name} image does not exist")
+
+
+
+def pack_all_files():
+    try:
+        bpy.ops.file.pack_all()
+    except:
+        print("no external files!!")
+
 
 def HasBlend(folder):
     # Loop through the folder and its subfolders
@@ -33,6 +65,14 @@ def HasBlend(folder):
                 print(file)
                 return True
     return False
+    
+def BlendFileCount(folder):
+    count = 0
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.lower().endswith(".blend"):
+                count += 1
+    return count
 
 def CopyFolderStructure(source, target):
     global LoadedFiles
@@ -59,14 +99,18 @@ def CopyFolderStructure(source, target):
             CopyFolderStructure(SourcePath,TargetPath)
         # If the source path is a file, copy it to the target path
         elif file.lower().endswith(".blend"):
-            
+            PreJpgCompression = True
             try:
                 bpy.ops.wm.open_mainfile(filepath=os.path.join(SourcePath))
+                pack_all_files()
+                if JpgCompression:
+                    convert_packed_images_to_jpg()
+                
                 LoadedFiles += 1
                 LoadedBytes += os.path.getsize(SourcePath)
             except RuntimeError as error:
                 print(error)
-                FailedSaves.append(TargetPath)
+                FailedSaves.append(SourcePath)
                 continue
             try:
                 
@@ -81,7 +125,7 @@ def CopyFolderStructure(source, target):
                     SavedBytes += os.path.getsize(TargetPath)
                     SavedFiles += 1            
                 except:
-                    FailedSaves.append(TargetPath)
+                    FailedSaves.append(SourcePath)
 
 CopyFolderStructure(SourceDir,DestDir)
 
@@ -95,11 +139,14 @@ if len(FailedSaves) != 0:
     for fail in FailedSaves:
         print(fail)
     print("Above mentioned files failed to compress/load")
+    print("-------------------------")
 
 SaveMB = round(SavedBytes/(1000*1000),1)
 LoadMB = round(LoadedBytes/(1000*1000),1)
 
-print(f'Source folder {LoadMB} MB -> Destination folder {SaveMB} MB')
-print(f'Loaded Source files : {LoadedFiles} Compressed files : {SavedFiles}')
+print(f'{LoadMB} MB -> {SaveMB} MB')
+print(f'Source Files : {BlendFileCount(SourceDir)}')
+print(f'Loaded Source files : {LoadedFiles}')
+print(f'Compressed files : {SavedFiles}')
 print("-------------------------")
 os._exit(1)
--- compressor.py
+++ compressor.py
@@ -8,16 +8,17 @@ import subprocess
 FileName = "dirs.pkl"
 CurrentDir = os.path.dirname(os.path.abspath(__file__))
 PicklePath = os.path.join(CurrentDir, FileName)
-def Save(a,b,c):
-    dirs = {"source": a, "dest": b,"blend": c}
+def Save(a,b,c,d):
+    dirs = {"source": a, "dest": b,"blend": c, "jpg": d} # add a key for jpg compression
     with open(PicklePath, "wb") as f:
         pickle.dump(dirs, f)
-    print(f"saved {a},{b},{c}")
+    print(f"saved {a},{b},{c},{d}") # print the jpg value as well
 
 def Compress():
     SourceDir = SourceEntry.get()
     DestDir = DestEntry.get()
     BlenderPath = BlenderEntry.get() # get the blender path from the entry
+    JpgValue = JpgVar.get() # get the jpg value from the variable
     if not os.path.isdir(SourceDir):
         mb.showerror("Error", "Invalid source directory")
         return
@@ -29,7 +30,7 @@ def Compress():
         return
     ScriptPath = os.path.join(CurrentDir, "BlenderHeadlessCompressor.py")
     try:
-        process = subprocess.Popen([BlenderPath, "--background", "--python", ScriptPath]) # use the blender path instead of "blender"
+        process = subprocess.Popen([BlenderPath, "--background", "--python", ScriptPath, str(JpgValue)]) # pass the jpg value as an argument to the script
         process.wait () 
         ReturnCode = process.returncode 
         print (ReturnCode) 
@@ -52,23 +53,30 @@ BlenderLabel = tk.Label(root, text="Blender.exe path:") # add a label for the bl
 BlenderLabel.grid(row=2, column=0, padx=5, pady=5)
 BlenderEntry = tk.Entry(root, width=40) # add an entry for the blender path
 BlenderEntry.grid(row=2, column=1, padx=5, pady=5)
-BrowseSourceButton = tk.Button(root, text="Browse", command=lambda: [SourceEntry.delete(0, tk.END), SourceEntry.insert(0, fd.askdirectory()),Save(SourceEntry.get(),DestEntry.get(),BlenderEntry.get())]) 
+JpgLabel = tk.Label(root, text="Convert Packed Images to jpg compression:") # add a label for the jpg compression
+JpgLabel.grid(row=3, column=0, padx=5, pady=5)
+JpgVar = tk.BooleanVar() # create a boolean variable for the jpg compression
+JpgCheck = tk.Checkbutton(root, text="Enable", variable=JpgVar) # create a checkbutton for the jpg compression
+JpgCheck.grid(row=3, column=1, padx=5, pady=5)
+BrowseSourceButton = tk.Button(root, text="Browse", command=lambda: [SourceEntry.delete(0, tk.END), SourceEntry.insert(0, fd.askdirectory()),Save(SourceEntry.get(),DestEntry.get(),BlenderEntry.get(),JpgVar.get())]) # save the jpg value as well
 BrowseSourceButton.grid(row=0, column=2, padx=5, pady=5)
-BrowseDestButton = tk.Button(root, text="Browse", command=lambda: [DestEntry.delete(0, tk.END), DestEntry.insert(0, fd.askdirectory()),Save(SourceEntry.get(),DestEntry.get(),BlenderEntry.get())]) 
+BrowseDestButton = tk.Button(root, text="Browse", command=lambda: [DestEntry.delete(0, tk.END), DestEntry.insert(0, fd.askdirectory()),Save(SourceEntry.get(),DestEntry.get(),BlenderEntry.get(),JpgVar.get())]) # save the jpg value as well
 BrowseDestButton.grid(row=1, column=2, padx=5, pady=5)
-BrowseBlenderButton = tk.Button(root, text="Browse", command=lambda: [BlenderEntry.delete(0, tk.END), BlenderEntry.insert(0, fd.askopenfilename()),Save(SourceEntry.get(),DestEntry.get(),BlenderEntry.get())]) # add a button to browse for the blender file
+BrowseBlenderButton = tk.Button(root, text="Browse", command=lambda: [BlenderEntry.delete(0, tk.END), BlenderEntry.insert(0, fd.askopenfilename()),Save(SourceEntry.get(),DestEntry.get(),BlenderEntry.get(),JpgVar.get())]) # save the jpg value as well
 BrowseBlenderButton.grid(row=2, column=2, padx=5, pady=5)
 CompressButton = tk.Button(root, text="Compress", command=Compress) 
-CompressButton.grid(row=3, column=1, padx=5, pady=5)
+CompressButton.grid(row=4, column=1, padx=5, pady=5) # change the row to 4
 try:
     with open(PicklePath, "rb") as f:
         dirs = pickle.load(f)
     SourceDir = dirs["source"]
     DestDir = dirs["dest"]
     BlendDir = dirs["blend"]
+    JpgDir = dirs["jpg"] # load the jpg value from the pickle file
     SourceEntry.insert(0, SourceDir)
     DestEntry.insert(0, DestDir)
     BlenderEntry.insert(0, BlendDir)
+    JpgVar.set(JpgDir) # set the checkbutton according to the jpg value
 except Exception as e:
     print(e)
 root.mainloop()
